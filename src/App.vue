a6b2e286932d98f4837612337954affc9112cdb9
Jeffrey <jeffreywangxiao@gmail.com>
2018-08-22 11:35:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <simple-flowchart msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleFlowchart from './components/SimpleFlowchart.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SimpleFlowchart
   }
 }
 </script>
