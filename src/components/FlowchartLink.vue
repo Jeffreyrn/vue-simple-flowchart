0880ce1e035b5d0a125ecc4888873dac657efc4f
Jeffrey <jeffreywangxiao@gmail.com>
2018-08-28 11:17:23 +0800

    add arrow to link

--- src/components/FlowchartLink.vue
+++ src/components/FlowchartLink.vue
@@ -2,7 +2,7 @@
   <g>
     <path :d="dAttr" :style="pathStyle"></path>
     <path d="M -1 -1 L 0 1 L 1 -1 z"
-      :style="pathStyle"
+      :style="arrowStyle"
       :transform="arrowTransform"></path>
   </g>
 </template>
@@ -34,8 +34,24 @@ export default {
         fill: 'none',
       }
     },
+    arrowStyle() {
+      return {
+        stroke: 'rgb(255, 136, 85)',
+        strokeWidth: 5.73205,
+        fill: 'none',
+      }
+    },
     arrowTransform() {
-      return 'translate(1, 1) rotate(0)'
+      // caculate arrow position: the center point between start and end
+      const dx = (this.end[0] - this.start[0]) / 2;
+      const dy = (this.end[1] - this.start[1]) / 2;
+      const [arrowX, arrowY] = [this.start[0] + dx, this.start[1] + dy];
+      // caculate arrow rotation
+      const angle = -Math.atan2(this.end[0] - this.start[0], this.end[1] - this.start[1]);
+      let degree = angle * 180 / Math.PI;
+      degree = degree < 0 ? degree + 360 : degree;
+      
+      return `translate(${arrowX}, ${arrowY}) rotate(${degree})`;
     },
     dAttr() {
       let cx = this.start[0], cy = this.start[1], ex = this.end[0], ey = this.end[1];
